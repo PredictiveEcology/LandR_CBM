9ef8578119ef76ec904a58b69c9878206e462117
Celine <cboisvenue@gmail.com>
2022-10-07 10:59:48 -0700

    preliminary work on LandRCBM_split3pools module

---
+++ scripts/miscCeline.r
@@ -0,0 +1,5 @@
+## trying to find the growth curves
+linkSpsTble <- "C:/Celine/Syncdocs/RES_Work/Work/LandRCBM/data/factorialSpeciesTable.Rdat"
+linkCurves <- "C:/Celine/Syncdocs/RES_Work/Work/LandRCBM/data/reducedFactorialCD.Rdat"
+spsTbl <- readRDS(linkSpsTble)
+curves <- readRDS(linkCurves)
