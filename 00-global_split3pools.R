1313075bf7b4bf5fbabaad8e09b805cdbaa17f14
Alex Chubaty <achubaty@for-cast.ca>
2022-12-07 10:27:20 -0700

    cleanup + comments for main splitpools script

--- 00-global_split3pools.R
+++ 00-global_split3pools.R
@@ -3,13 +3,20 @@
 if (file.exists("~/.Renviron")) readRenviron("~/.Renviron") ## GITHUB_PAT
 if (file.exists("LandR_CBM.Renviron")) readRenviron("LandR_CBM.Renviron") ## database credentials
 
-.ncores <- min(parallel::detectCores() / 2, 32L)
-.nodename <- Sys.info()[["nodename"]]
-.starttime <- Sys.time()
-.user <- Sys.info()[["user"]]
+.ncores <- min(parallel::detectCores() / 2, 32L) ## default number of CPU cores to use, e.g. for pkg install
+.nodename <- Sys.info()[["nodename"]] ## current computer name; used to configure machine-specific settings
+.user <- Sys.info()[["user"]] ## current computer username; used to configure user-specific settings
 
-prjDir <- "~/GitHub/LandR_CBM"
+## define project directory - this code expects it is being run from this location
+## **do not change the paths defined here**
+## if you need to add a machine- or user-specific path, please do so _conditionally_
+prjDir <- if (.user == "cboisven") {
+  "C:/Celine/github/LandR_CBM"
+} else {
+  "~/GitHub/LandR_CBM"
+}
 
+## ensure script being run from the project directory
 stopifnot(identical(normalizePath(prjDir), normalizePath(getwd())))
 
 options(
@@ -18,7 +25,7 @@ options(
   Require.RPackageCache = "default" ## will use default package cache directory: `RequirePkgCacheDir()`
 )
 
-## this is a work-around for working from PFC...R cannot connect to URL
+## work-around for working from PFC...R cannot connect to certain urls
 ## TODO: improve conditional by only using wininet if *at* PFC, not just on a PFC machine
 if ((.Platform$OS.type == "windows") && grepl("[L|W]-VIC", .nodename)) {
   options("download.file.method" = "wininet")
@@ -26,17 +33,14 @@ if ((.Platform$OS.type == "windows") && grepl("[L|W]-VIC", .nodename)) {
 
 # install and load packages -------------------------------------------------------------------
 
+## use project-specific location for packages to avoid conflicts with other projects
 pkgDir <- file.path(tools::R_user_dir(basename(prjDir), "data"), "packages",
                     version$platform, getRversion()[, 1:2])
 dir.create(pkgDir, recursive = TRUE, showWarnings = FALSE)
 .libPaths(pkgDir, include.site = FALSE)
 message("Using libPaths:\n", paste(.libPaths(), collapse = "\n"))
 
-### In this section, only load the minimum of packages (Require, SpaDES.install)
-#so all packages can be installed with correct version numbering. If we load a
-#package too early and it is an older version that what may be required by a
-#module, then we get an inconsistency
-
+## package installation only; do not load module packages until after install
 if (!"remotes" %in% rownames(installed.packages(lib.loc = .libPaths()[1]))) {
   install.packages("remotes")
 }
@@ -50,6 +54,8 @@ if (!"Require" %in% rownames(installed.packages(lib.loc = .libPaths()[1])) ||
 library(Require)
 
 ## temporarily until new Rcpp release on CRAN in early 2023 ----------------------------------------
+## this version fixes spurious error triggered in raster/terra
+## 'Error in x$.self$finalize() : attempt to apply non-function'
 options("Require.otherPkgs" = setdiff(getOption("Require.otherPkgs"), "Rcpp")) ## remove Rcpp from "forced source"
 RcppVersionNeeded <- package_version("1.0.9.3")
 
@@ -65,12 +71,13 @@ if (RcppVersionAvail < RcppVersionNeeded) {
 }
 ## end temporary Rcpp
 
-setLinuxBinaryRepo()
+setLinuxBinaryRepo() ## setup binary package installation for linux users
 
 Require(c(
+  "PredictiveEcology/reproducible@development (>= 1.2.14)",
   "PredictiveEcology/SpaDES.project@transition (>= 0.0.7.9003)", ## TODO: use development once merged
   # "PredictiveEcology/SpaDES.config@development (>= 0.0.2.9040)", ## TODO: use config
-  "PredictiveEcology/SpaDES.core@development (>= 1.1.0.9004)"
+  "PredictiveEcology/SpaDES.core@development (>= 1.1.0.9015)"
 ), upgrade = FALSE, standAlone = TRUE)
 
 modulePkgs <- unname(unlist(packagesInModules(modulePath = file.path(prjDir, "modules"))))
@@ -80,7 +87,7 @@ Require(unique(c(modulePkgs, otherPkgs)), require = FALSE, standAlone = TRUE, up
 
 ## NOTE: always load packages LAST, after installation above;
 ##       ensure plyr loaded before dplyr or there will be problems
-Require(c("data.table", "plyr", "pryr", "SpaDES.core",
+Require(c("data.table", "plyr", "pryr", "reproducible", "SpaDES.core",
           "googledrive", "httr", "magrittr", "sessioninfo", "slackr"),
         upgrade = FALSE, standAlone = TRUE)
 
@@ -98,6 +105,8 @@ paths <- list(
   scratchPath = file.path(dirname(tempdir()), "scratch", "LandR_CBM")
 ) ## TODO: use config
 
+do.call(SpaDES.core::setPaths, paths) ## set project paths for simulation
+
 # project options -----------------------------------------------------------------------------
 
 ## TODO: use config
@@ -109,27 +118,25 @@ quickPlot::dev.useRSGD(useRSGD = quickPlot::isRstudioServer())
 
 # begin simulations ---------------------------------------------------------------------------
 
-do.call(SpaDES.core::setPaths, paths)
-
 ## Study area RTM
 
 RIArtm <- Cache(prepInputs,
                 url = "https://drive.google.com/file/d/1h7gK44g64dwcoqhij24F2K54hs5e35Ci",
                 destinationPath = paths[["inputPath"]])
-##TODO
-### I think there needs to be this as a parameter for this module?
+
+## TODO: I think there needs to be this as a parameter for this module?
 #idCols <- c(“pixelGroup”, “cohort_id”)
 # this would need to trickly through the module
 
 # this is not working for me
-# options(spades.moduleCodeChecks = FALSE,
+# options(spades.moduleCodeChecks = FALSE, ## don't turn this off during development; use TRUE
 #         spades.recoveryMode = FALSE)
 # if (any(Sys.info()[["user"]] %in% c("cboisven", "cboisvenue"))) {
 #   Require("googledrive")
 #   options(
 #     gargle_oauth_cache = ".secrets",
 #     gargle_oauth_email = "cboisvenue@gmail.com"
-#   )
+#   ) ## you shouldn't need this unless you have a custom gargle/googledrive setup - config can deal with this
 # }
 
 objects <- list(rasterToMatch = RIArtm)
@@ -146,7 +153,8 @@ split3poolsInit <- simInit(
   times = times,
   params = parameters,
   modules = modules,
-  objects = objects
+  objects = objects#,
+  #paths = paths
 )
 
 # # Extras not used, for plotting some things
